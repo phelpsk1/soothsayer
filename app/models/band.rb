13defff097cb66b513e91b952f22aa0f4ec9137e
Kevin <kevin@kevin-VirtualBox.(none)>
2013-10-02 18:02:30 -0400

    working audio, added links to songs in bands page

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,3 +1,4 @@
 class Band < ActiveRecord::Base
+	has_many :songs, dependent: :destroy
 	has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ActiveRecord::Base
   has_attached_file :mp3,
-                    :url => '/system/:class/:id_partition/:style.:extension',
-                    :path => ':rails_root/public/system/:class/:id_partition/:style.:extension'
+                    :url => '/system/:class/:id_partition/:basename.:extension',
+                    :path => ':rails_root/public/system/:class/:id_partition/:basename.:extension'
 
   validates_attachment_presence :mp3
   validates_attachment_content_type :mp3, :content_type => [ 'application/mp3', 'application/x-mp3', 'audio/mpeg', 'audio/mp3' ]
