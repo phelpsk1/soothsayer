d3df428ae72f6f53ead6d3488127ce3f456430f7
Kevin <kevin@kevin-VirtualBox.(none)>
2013-10-02 01:20:04 -0400

    testing mp3

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,8 @@
 class Song < ActiveRecord::Base
-  has_attached_file :mp3,
-                    :url => '/:class/:id/:style.:extension',
-                    :path => ':rails_root/assets/:class/:id_partition/:style.:extension'
+  has_attached_file :mp3
+
+#                    :url => '/:class/:id/:style.:extension',
+#                    :path => ':rails_root/public/system/:class/:id_partition/:style.:extension'
 
   validates_attachment_presence :mp3
   validates_attachment_content_type :mp3, :content_type => [ 'application/mp3', 'application/x-mp3', 'audio/mpeg', 'audio/mp3' ]
