92ab5c93d044eb418255b85e107d4cef570005e2
Kevin <kevin@kevin-VirtualBox.(none)>
2013-12-12 06:44:55 -0500

    forcing migration

--- db/migrate/20131212083529_create_gigs.rb
+++ db/migrate/20131212083529_create_gigs.rb
@@ -1,6 +1,6 @@
 class CreateGigs < ActiveRecord::Migration
   def change
-    create_table :gigs do |t|
+    create_table :gigs, force:true do |t|
 			t.belongs_to :venue
 			t.datetime :start_time
 			t.string :title
